432bf44cd99b45eb74c16b48010869051602e56f
lxulxu555 <50168605+lxulxu555@users.noreply.github.com>
2022-10-21 10:14:32 +0800

    Update .fatherrc.ts

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,29 @@
 export default {
-  esm: 'rollup',
-  cjs: 'rollup',
+  esm: {
+   // input: 'src', // 默认编译目录
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+  },
+  cjs: {
+    //input: 'src', // 默认编译目录
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+  },
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src/',
+        rootPathPrefix: '@/',
+      },
+    ],
+  ],
 };
